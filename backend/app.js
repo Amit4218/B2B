fce585c54948283e578f7e716b997a95a785aaa1
Amit Bhagat <amitbhagat621@gmail.com>
2025-09-12 17:17:16 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -16,7 +16,7 @@ const app = express();
 const server = createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: [`${process.env.FRONTEND_SOCKET_URL}`],
+    origin: `${process.env.FRONTEND_SOCKET_URL}`,
     // credentials: true,
   },
 });
